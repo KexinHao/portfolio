aae58cf18bb611b50ad848f56c50436766bc5a2d
Kexin <kexinhao01111993@gmail.com>
2025-06-17 18:07:51 +0200

    major website renovation
    renovated the about page and added a new page called news&agenda. designed a new look.

---
+++ Javascript/about.js
@@ -0,0 +1,9 @@
+function showEnglish() {
+  document.querySelector('.english').style.display = 'block';
+  document.querySelector('.chinese').style.display = 'none';
+}
+
+function showChinese() {
+  document.querySelector('.english').style.display = 'none';
+  document.querySelector('.chinese').style.display = 'block';
+}
